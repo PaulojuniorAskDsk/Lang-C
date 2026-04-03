1ea357469dd747f43c5317ac7bc04bcc7e6ad71f
PaulojuniorAskDsk <paulohandersonlsjunior@gmail.com>
2026-04-03 00:05:58 -0300

    commit for classes

---
+++ 1- hello wolrd.C
@@ -0,0 +1,5 @@
+#include <stdio.h>//library for input and output functions
+int main() {//main function - entry point of the program
+    printf("Hello, World!\n");//print "Hello, World!" to the console followed by a new line
+    return 0;//return 0 indicates that the program ended successfully
+}
---
+++ 2- variables and data types.c
@@ -0,0 +1,16 @@
+#include   <stdio.h>
+int main() {
+    //who is declaring the variable and what is the type of the variable
+    //type variable_name = value;
+    int age = 30; //integer variable
+    float height = 1.75; //floating-point variable
+    double weight = 70.5; //double variable
+    // a diference between float and double is that double has a higher precision than float
+    char grade = 'A'; //character variable
+    char name[] = "John"; //string variable (array of characters)
+    return 0;
+    //rules of variable declaration
+    //1. variable name must start with a letter or an underscore
+    //2. variable name can only contain letters, digits, and underscores
+    //3. variable name cannot be a reserved keyword
+}
