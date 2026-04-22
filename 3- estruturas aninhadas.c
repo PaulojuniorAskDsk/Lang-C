374ca9182fed912d7f8e6b5ad35f49dea149eb40
PaulojuniorAskDsk <Paulohandersonlsjunior@gmail.ccom>
2026-04-22 20:55:20 -0300

    aulas

---
+++ 3- estruturas aninhadas.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    //estruturas de decisão aninhadas
+if (condicao1) {
+  if (condicao2) {
+    // Código a ser executado se condicao1 e condicao2 forem verdadeiras
+  }
+}
+  return 0;
+}
